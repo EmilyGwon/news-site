dfdd2a2212a38f68d4dbd5a6c868b0f1c75e29d7
emilygwon <gygwon@fastviewkorea.com>
2026-07-19 15:14:13 +0900

    Initial commit: TV리포트 리라이트 사이트 (정적 배포)

---
+++ config.js
@@ -0,0 +1,6 @@
+﻿// Supabase 연결 정보 (자동 주입됨)
+window.SUPA = {
+  url: "https://pxmmpowleruxejlodnfs.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bW1wb3dsZXJ1eGVqbG9kbmZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0MDU4MTAsImV4cCI6MjA5OTk4MTgxMH0.s5RHWhCqij0LET9CemzS5VT3ZlbKikV3vx3NEcyk7C4"
+};
+
